9b0ecb64295417a538ed7224c54e309f482e3b6f
Bryan <blindaa121@gmail.com>
2020-09-08 11:54:40 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARDjjEsovT2TEFR4hH2PzUsK5YrQ8-7J0",
+    authDomain: "challenge-b2d7f.firebaseapp.com",
+    databaseURL: "https://challenge-b2d7f.firebaseio.com",
+    projectId: "challenge-b2d7f",
+    storageBucket: "challenge-b2d7f.appspot.com",
+    messagingSenderId: "1048721185294",
+    appId: "1:1048721185294:web:8464ead854781c4490fa5f"
+};
+
